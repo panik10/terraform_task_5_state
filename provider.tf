c97653575ab38d93cc27a8437f3968d72b1cef5e
Pavlo Nikolaiev <workhardpolly@gmail.com>
2025-05-06 18:11:15 +0200

    solution

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.25.0"
+    }
+  }
+
+  backend "azurerm" {
+    storage_account_name = "storagemate34"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    resource_group_name  = "tfstate"
+    use_oidc             = true
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+}
